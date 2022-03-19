4baf5f1027b5eec464f22842b9d3491e4d5639df
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-19 20:40:26 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -13,9 +13,9 @@ LIGHT='\033[0;37m'
 # ==========================================
 clear
 apt install jq curl -y
-DOMAIN=vpsku.space
+DOMAIN=sumbawa.me
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-SUB_DOMAIN=${sub}.vpsku.space
+SUB_DOMAIN=${sub}.sumbawa.me
 CF_ID=paoandest@gmail.com
 CF_KEY=1d158d0efc4eef787222cefff0b6d20981462
 set -euo pipefail
