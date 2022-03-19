7065114378c4785cc63dc345fd68ba2dc8cc8722
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-03-19 17:15:35 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+clear
+echo -e "\e[0m                                                                            "
+echo -e "\e[92m                     █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█                                "
+echo -e "\e[92m                     █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█                                "
+echo -e "\e[92m                     █░░║║║╠─║─║─║║║║║╠─░░█                                "
+echo -e "\e[92m                     █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█                                "
+echo -e "\e[92m                     █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█                                "
+echo -e "\e[0m                                                                            "
+echo -e "\e[93m                       AutoScript VPNshopee                                "
+echo -e "\e[0m                                                                            "
+echo -e "\e[93m                         Script by ANU                                    "
+echo -e "\e[93m                          [1] BACK MENU                                    "
+echo -e "\e[93m        .----------------------------------------------------.             "
+echo -e "\e[93m        |             Script status : Premium                |             "
+echo -e "\e[93m        '----------------------------------------------------'             "
+read -p "               Select an option [1] than press ENTER: :  " Options
+sleep 1
+clear
+case $Options in
+		1)
+		clear
+		menu
+		exit
+		;;
+	esac
